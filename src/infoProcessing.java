7b2225cbfd9911818859b5bcfee55598a0726de7
Daniel Buchanan <dsbuchan@ncsu.edu>
2023-11-09 17:16:09 -0500

    Working on implementing infoProcessing

--- src/infoProcessing.java
+++ src/infoProcessing.java
@@ -22,7 +22,7 @@ public class infoProcessing {
         System.out.print("\nEnter university id: ");
         String id = sc.nextLine();
         sc.close();
-        String query = String.format("INSERT INTO Driver VALUES(%s, %s, %s, %s);", phoneNumber, name, status, id);
+        String query = String.format("INSERT INTO Driver (phone, name, status, univ_id) VALUES(%s, %s, %s, %s);", phoneNumber, name, status, id);
         try{
             statement.executeUpdate(query);
             System.out.println("Driver Added");
@@ -32,15 +32,81 @@ public class infoProcessing {
        
     }
 
-    // public void updateDriverInfo(Statement statement){
-    //     Scanner sc = new Scanner(System.in);
-    //     System.out.print("Enter phone number: ");
-    //     String phoneNumber = sc.nextLine();
-    //     System.out.print("\nUpdate status? (y/n): ");
-    //     if(sc.next() == "y"){
-    //         System.out.print("\nEnter status : ");
-    //     }
-    //     sc.close();
+    public void updateDriverInfo(Statement statement){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter phone number: ");
+        String phoneNumber = sc.nextLine();
+        System.out.print("\nUpdate status? (y/n): ");
+        if(sc.next() == "y"){
+            System.out.print("\nEnter status : ");
+        }
+        sc.close();
 
-    // }
+    }
+
+    public void deleteDriverInfo(Statement statement){
+        Scanner sc = new Scanner(System.in);
+        int option = -1;
+        String phoneNumber = null;
+        String id = null;
+        do{
+            System.out.print("Delete Driver by (1) phone or (2) university id");
+            option = sc.nextInt();
+            if (option == 1){
+                System.out.print("\nEnter phone number: ");
+                phoneNumber = sc.nextLine();
+                try{
+                    statement.executeUpdate(String.format("DELETE FROM Driver WHERE phone = %s;", phoneNumber));
+                    System.out.println("Driver Deleted");
+                } catch (Throwable oops) {
+                    oops.printStackTrace();
+                }
+
+            }
+            if (option == 2){
+                System.out.print("\nEnter id: ");
+                id = sc.nextLine();
+                try{
+                    statement.executeUpdate(String.format("DELETE FROM Driver WHERE univ_id = %s;", id));
+                    System.out.println("Driver Deleted");
+                } catch (Throwable oops) {
+                    oops.printStackTrace();
+                }
+            }
+        } while (option != 1 && option != 2);
+        sc.close();
+    }
+
+    public void enterParkingLotInfo(Statement statement){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter lot name: ");
+        String lot_name = sc.nextLine();
+        System.out.print("\nEnter address: ");
+        String address = sc.nextLine();
+        sc.close();
+        String query = String.format("INSERT INTO ParkingLot (lot_name, address) VALUES(%s, %s);", lot_name, address);
+        try{
+            statement.executeUpdate(query);
+            System.out.println("Parking Lot Added");
+        } catch (Throwable oops) {
+			oops.printStackTrace();
+		}
+    }
+
+    public void updateParkingLotInfo(Statement statement){
+        //TODO implement
+    }
+
+    public void deleteParkingLotInfo(Statement statement){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter lot name: ");
+        String lot_name = sc.nextLine();
+        sc.close();
+        try{
+            statement.executeUpdate(String.format("DELETE FROM ParkingLot WHERE lot_name = %s;", lot_name));
+            System.out.println("Parking Lot Deleted");
+        } catch (Throwable oops) {
+            oops.printStackTrace();
+        }
+    }
 }
--- test/infoProcessingTest.java
+++ test/infoProcessingTest.java
@@ -1,19 +1,80 @@
 package test;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.junit.jupiter.api;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.*;
+import java.util.Properties;
+import java.util.Scanner;
 
 import src.infoProcessing;
 
-
 public class infoProcessingTest {
+    public static Connection connection = null;
+    public static Statement statement = null;
+    public static String user = null;
+    public static String password = null;
+    private static Scanner sc = new Scanner(System.in);
 
+    @BeforeEach
+    public void initiateConnection(){
+        getUser();
+        connectToDatabase("jdbc:mariadb://classdb2.csc.ncsu.edu:3306/" + user, user, password);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Shutting down...");
+            close();
+        }));
+    }
+    
     @Test
     public void testEnterDriverInfo(){
         Statement statement = null;
+
         infoProcessing in = new infoProcessing();
         in.enterDriverInfo(statement);
         assertEquals(1,1);
     }
 
+
+
+    static void getUser() {
+		Properties properties = new Properties();
+        FileInputStream input = null;
+        try {
+            input = new FileInputStream("../db_keys");
+            properties.load(input);
+            user = properties.getProperty("username");
+            password = properties.getProperty("password");
+            System.out.println("User Found");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+
+    static void connectToDatabase(String jdbcURL, String user, String password) throws ClassNotFoundException, SQLException {
+        Class.forName("org.mariadb.jdbc.Driver");
+        connection = DriverManager.getConnection(jdbcURL, user, password);
+        statement = connection.createStatement();
+        System.out.println("Connected to Database");
+    }
+
+    static void close() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Database Connection Terminated");
+    }
 }
